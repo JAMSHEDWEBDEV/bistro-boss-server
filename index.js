6c92383e5d00bcc038e0afb71d13edc21c5406a9
JAMSHEDWEBDEV <jamsheddc60@gmail.com>
2023-11-16 02:03:01 +0600

    added post cart data to database

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ async function run() {
     
     const menuCollection = client.db('bistroDB').collection('menu');
     const reviewCollection = client.db('bistroDB').collection('reviews');
+    const cartCollection = client.db('bistroDB').collection('carts');
 
     app.get('/menu', async(req,res)=>{
         const result = await menuCollection.find().toArray();
@@ -36,6 +37,12 @@ async function run() {
     app.get('/reviews', async(req,res)=>{
         const result = await reviewCollection.find().toArray();
         res.send(result);
+    })
+    // cart data post 
+    app.post('/carts', async(req,res)=>{
+      const cartData = req.body;
+      const result = await cartCollection.insertOne(cartData);
+      res.send(result);
     })
 
 
